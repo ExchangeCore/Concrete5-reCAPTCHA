01686b2043f1501afefd6fcc097123c98ecf4d67
Chris Hougard <chris@exchangecore.com>
2014-12-10 22:01:55 -0600

    Added noscript fallback

--- src/Captcha/RecaptchaController.php
+++ src/Captcha/RecaptchaController.php
@@ -30,6 +30,26 @@ class RecaptchaController extends CaptchaController
         $config = Package::getByHandle('ec_recaptcha')->getConfig();
         echo '<script src="https://www.google.com/recaptcha/api.js"></script>';
         echo '<div class="g-recaptcha" data-sitekey="' . $config->get('captcha.site_key') . '"></div>';
+        echo '<noscript>
+          <div style="width: 302px; height: 352px;">
+            <div style="width: 302px; height: 352px; position: relative;">
+              <div style="width: 302px; height: 352px; position: absolute;">
+                <iframe src="https://www.google.com/recaptcha/api/fallback?k=' . $config->get('captcha.site_key') . '"
+                        frameborder="0" scrolling="no"
+                        style="width: 302px; height:352px; border-style: none;">
+                </iframe>
+              </div>
+              <div style="width: 250px; height: 80px; position: absolute; border-style: none;
+                          bottom: 21px; left: 25px; margin: 0; padding: 0; right: 25px;">
+                <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+                          class="g-recaptcha-response"
+                          style="width: 250px; height: 80px; border: 1px solid #c1c1c1;
+                                 margin: 0; padding: 0; resize: none;" value="">
+                </textarea>
+              </div>
+            </div>
+          </div>
+        </noscript>';
     }
 
     /**
